d7af06a13d70f71528e3863d56fb9c487bfb50e7
Your Name <andreas.nussli@gmail.com>
2024-05-21 14:13:17 +0200

    Created proper folder structure -under my name (anu) there is project, filesheare and within project a backup folder

--- Andreas_Nuessli/PS-Projekt_AV_17.05/PS-Projekt_16.05/MaxMustermann.ps1
+++
@@ -1,8 +0,0 @@
-﻿New-ADUser -GivenName "Max" `
-           -Surname "Mustermann" `
-           -Name "Max Mustermann" `
-           -SamAccountName "mmustermann" `
-           -Path "OU=BZTF,DC=bztf,DC=local" `
-           -Enabled $True `
-           -AccountPassword (ConvertTo-SecureString "bztf.001" -AsPlainText -Force) `
-           -ChangePasswordAtLogon $False
