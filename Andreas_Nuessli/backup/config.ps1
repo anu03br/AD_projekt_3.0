05561d0906fab566341fc4c3068286c4ae64f076
Your Name <andreas.nussli@gmail.com>
2024-05-27 15:57:37 +0200

    nope

---
+++ Andreas_Nuessli/backup/config.ps1
@@ -0,0 +1,27 @@
+﻿#--------------------------------------------------------------------------------
+# Autor: Amar Vejapi
+# Funktion des Skripts: Ganzes Skript
+# Funktion des Skripts: Konfigurationen, z.B. Pfade bereitstellen
+# Datum: 15.05.2024
+# Version: 0.5
+# Changelog
+# 15.05.24 V0.5 config file erstellt
+# 21.05.24 V0.6 Umbenennung config file von "Projekt Skript zu "config.ps1"
+# 21.05.24 V0.6 Variablen LogFileUser, LogFileGroup, ClassFolder mit relativen statt absoluten pfaden versehen
+#--------------------------------------------------------------------------------
+
+# HashTable der Variable $config mit den Keys und Values.
+$config = @{
+    SchuelerCsv  = ".\schueler-klein.csv"
+    InitPw       = "bztf.001"
+    OUPath       = "OU=BZTF,DC=bztf,DC=local"
+    OULernende   = "Lernende"
+    OUKlasse     = "Klassengruppen"
+    LogFileUser  = ".\\users.log"
+    LogFileGroup = ".\\groups.log"
+    ClassFolder  = ".\\Klassen"
+    # Renamed extensionAttribute
+    Klasse          = "extensionAttribute1"  # Custom attribute name $Klasse
+    Klasse2         = "extensionAttribute2"  # Custom attribute name $Klasse2
+    DisableDate     = "extensionAttribute3"  # Custom attribute name $disableDate
+}
